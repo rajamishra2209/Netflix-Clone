3d989d9ca2d8edc46e68b0d6c34405e0bc0348b8
rajamishra2209 <rajamishra220900@gmail.com>
2023-10-18 05:53:44 +0530

    Modified

--- netflix-ui/src/pages/Netflix.jsx
+++ netflix-ui/src/pages/Netflix.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import Navbar from "../components/Navbar"
 export default function Netflix() {
+  const [isScrolled,setIsScrolled]= useState(false);
+  
+  //Registering an event listener here
+  window.onscroll =() =>{
+   setIsScrolled(window.pageXOffset===0?false:true);
+   return ()=> (window.onscroll= null);
+  }
   return (
-    <div>Netflix</div>
+    <Navbar isScrolled={isScrolled}/>
   )
 }
 
