8b64cfba7f600a78b99c9d017de4be441426c4e7
rajamishra2209 <rajamishra220900@gmail.com>
2023-10-18 05:52:33 +0530

    Created Navbar Component

---
+++ netflix-ui/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+function Navbar() {
+  const links = [
+    { name: "Home", link: "/" },
+    { name: "TV Shows", link: "/tv" },
+    { name: "Movies", link: "/movies" },
+    { name: "My List", link: "/mylist" },
+  ];
+  return <Container>Navbar</Container>;
+}
+const Container =styled.div``;
+export default Navbar;
